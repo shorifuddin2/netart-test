4fd5c3cfc9a839abdbc5961d3b19aa91d17b6e9b
shorifuddin2 <shorifuddin240915@gmail.com>
2022-12-04 00:06:25 +0600

    logo set

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Instrument from './asserts/Instrument.png';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Container">
       <div className='logo'>
-      <img id='logo' src={logo} alt='Logo'/>
+      <img className='logo' src={logo} alt='Logo'/>
       </div>
 
       
@@ -16,11 +16,15 @@ function App() {
             <div className='col-6'>
             <img className='Trope' src={Trope} alt='trope'/>
             </div>
-            <div className='col-6'>
+            <div className='Awa'>
+              <p>C.R.I.PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</p>
+              <p>
+                <ol>C.R.I's energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</ol>
+                <ol>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Service Limited) to replace the old inefficient pumps With 5 Star rated energy efficient smart pumps With IoT enabled control panel.</ol>
+              </p>
             <img className='Award' src={Award} alt='trope'/>
             </div>
         </div>
-      
       
 
       <div className='InstrumentAria'>
